32959383f834942715d0e9d9b4602ed47644a3c4
Andres Suarez <zertosh@gmail.com>
2015-05-24 00:13:48 -0400

    Optimize the walker function
    * Use for-in loop instead of `Object.keys`
    * Skip a node's property early if is it's not an Object
      - Avoids the costly `Array.isArray()` check on non-nodes
    * Do the `typeof child.type === 'string'` check before `Array.isArray`
      - Since most children aren't arrays, we can avoid the expensive
      	`Array.isArray`
    * Use `Array.isArray` directly

--- index.js
+++ index.js
@@ -17,13 +17,17 @@ module.exports = function (src) {
 };
 
 function walk (node, parent, cb) {
-    var keys = objectKeys(node);
-    for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
+    for (var key in node) {
+        if (!node.hasOwnProperty(key)) continue;
+        if (typeof node[key] !== 'object' || !node[key]) continue;
         if (key === 'parent') continue;
         
         var child = node[key];
-        if (isArray(child)) {
+        if (typeof child.type === 'string') {
+            child.parent = node;
+            walk(child, node, cb);
+        }
+        else if (Array.isArray(child)) {
             for (var j = 0; j < child.length; j++) {
                 var c = child[j];
                 if (c && typeof c.type === 'string') {
@@ -32,20 +36,6 @@ function walk (node, parent, cb) {
                 }
             }
         }
-        else if (child && typeof child.type === 'string') {
-            child.parent = node;
-            walk(child, node, cb);
-        }
     }
     cb(node);
 }
-
-var isArray = Array.isArray || function (xs) {
-    return Object.prototype.toString.call(xs) === '[object Array]';
-};
-
-var objectKeys = Object.keys || function (obj) {
-    var keys = [];
-    for (var key in obj) keys.push(key);
-    return keys;
-};
